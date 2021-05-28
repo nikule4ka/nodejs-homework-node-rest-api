a4751816e1f5d7d8efda1d413830136fb3385bd5
Nika <64929516+nikule4ka@users.noreply.github.com>
2021-05-28 17:53:52 +0300

    edit all files done

---
+++ repositories/contacts.js
@@ -0,0 +1,48 @@
+const Contact = require('../model/contact')
+
+const listContacts = async () => {
+  const results = await Contact.find()
+  return results
+}
+
+const getContactById = async (contactId) => {
+  const result = await Contact.findOne({ _id: contactId })
+  return result
+}
+
+const removeContact = async (contactId) => {
+  const result = await Contact.findOneAndRemove({ _id: contactId })
+  return result
+}
+
+const addContact = async (body) => {
+  const result = await Contact.create(body)
+  return result
+}
+
+const updateContact = async (contactId, body) => {
+  const result = await Contact.findOneAndUpdate(
+    { _id: contactId },
+    { ...body },
+    { new: true }
+  )
+  return result
+}
+
+const updateStatusContact = async (contactId, body) => {
+  const result = await Contact.findByIdAndUpdate(
+    { _id: contactId },
+    { ...body },
+    { new: true }
+  )
+  return result
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+  updateStatusContact
+}
--- routes/api/validation.js
+++ routes/api/validation.js
@@ -1,45 +1,57 @@
-const Joi = require('joi');
+const Joi = require('joi')
+const mongoose = require('mongoose')
 
 const schemaCreateContact = Joi.object({
-    name: Joi.string()
-        .alphanum()
-        .min(3)
-        .max(30)
-        .required(),
-        email: Joi.string().email().optional(),
-        phone: Joi.string().pattern(/^\(\d{3}\)\s\d{3}-\d{4}/).optional()
-    }).or('name', 'email', 'phone')
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string().email().optional(),
+  phone: Joi.string()
+    .pattern(/^\(\d{3}\)\s\d{3}-\d{4}/)
+    .optional(),
+  favorite: Joi.boolean().optional()
+}).or('name', 'email', 'phone')
 
-    const schemaUpdateContact = Joi.object({
-        name: Joi.string()
-            .alphanum()
-            .min(3)
-            .max(30)
-            .required(),
-            email: Joi.string().email().optional(),
-            phone: Joi.string().pattern(/^\(\d{3}\)\s\d{3}-\d{4}/).optional()
-        }).or('name', 'email', 'phone')
-    
+const schemaUpdateContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string().email().optional(),
+  phone: Joi.string()
+    .pattern(/^\(\d{3}\)\s\d{3}-\d{4}/)
+    .optional(),
+  favorite: Joi.boolean().optional()
+}).or('name', 'email', 'phone')
+
+const schemaFavoriteContact = Joi.object({
+  favorite: Joi.boolean().required()
+})
 
 const validate = async (schema, obj, next) => {
-    try {
-         await schema.validateAsync(obj);
-        next()
-    } catch (err) { 
-        next({
-            status: 400,
-            message: err.message.replace(/"/g, '')
-        })
-    }
-} 
-
-module.exports = {
-    validationCreateContact: (req,res,next) => {
-        return validate(schemaCreateContact, req.body, next)
-    },
-    validationUpdateContact: (req,res,next) => {
-        return validate(schemaUpdateContact, req.body, next)
-    },
+  try {
+    await schema.validateAsync(obj)
+    next()
+  } catch (err) {
+    next({
+      status: 400,
+      message: err.message.replace(/"/g, '')
+    })
+  }
 }
 
-
+module.exports = {
+  validationCreateContact: (req, res, next) => {
+    return validate(schemaCreateContact, req.body, next)
+  },
+  validationUpdateContact: (req, res, next) => {
+    return validate(schemaUpdateContact, req.body, next)
+  },
+  validationFavoriteContact: (req, res, next) => {
+    return validate(schemaFavoriteContact, req.body, next)
+  },
+  validateMongoId: (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return next({
+        status: 400,
+        message: 'Invalid ObjectId'
+      })
+    }
+    next()
+  }
+}
