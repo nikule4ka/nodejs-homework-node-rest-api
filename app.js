1ec13d9ae904c23ff5f3391f471ce7d48247ea31
Nika <64929516+nikule4ka@users.noreply.github.com>
2021-06-13 20:02:46 +0300

    edit all changes

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const boolParser = require('express-query-boolean');
 const { limiterApi } = require('./helpers/constans');
+const path = require('path')
+
+require('dotenv').config()
+const AVATAR_OF_USERS = process.env.AVATAR_OF_USERS
 
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(helmet());
+app.use(helmet())
+app.use(express.static(path.join(__dirname, AVATAR_OF_USERS)))
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json({ limit: 10000 }));
+app.use(express.json({ limit: 10000 }))
 app.use(boolParser());
-app.use('/api/', rateLimit(limiterApi));
-app.use('/api/', require('./routes/api'));
+app.use('/api/', rateLimit(limiterApi))
+app.use('/api/', require('./routes/api'))
 
 
 app.use((req, res) => {
--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 const Users = require('../repositories/users');
 const { HttpCode } = require('../helpers/constans');
 const jwt = require('jsonwebtoken');
+const fs = require('fs/promises')
+const path = require('path')
 require('dotenv').config();
 const UploadAvatarService = require('../services/local-upload')
 const SECRET_KEY = process.env.SECRET_KEY;
@@ -107,7 +109,12 @@ const avatars = async (req, res, next) => {
     const id = req.user.id
     const uploads = new UploadAvatarService(process.env.AVATAR_OF_USERS)
     const avatarUrl = await uploads.saveAvatar({ idUser: id, file: req.file })
-    // TODO: need delete old avatar
+    // replacement old avatar
+    try {
+      await fs.unlink(path.join(process.env.AVATAR_OF_USERS, req.user.avatar))
+    } catch (error) {
+      console.log(error.message)
+    }
 
     await Users.updateAvatar(id, avatarUrl)
     res.json({ status: 'success', code: HttpCode.OK, user: { avatarUrl } })
