1f72b7b783e911ff6d4b0ff213dde1b92328908d
Nika <64929516+nikule4ka@users.noreply.github.com>
2021-06-14 18:22:15 +0300

    edit guard test

--- test/guard.test.js
+++ test/guard.test.js
@@ -1,11 +1,56 @@
 const guard = require('../helpers/guard')
+const { HttpCode } = require('../helpers//constans')
 
-describe('Unit test controller contacts', () => {
-  const req = { user: { id: 1 }, body: {}, params: { id: 1 } }
+const passport = require('passport')
+
+describe('Unit test guard', () => {
+  const user = { token: '1122565665656' }
+  const req = { get: jest.fn((header) => `Bearer ${user.token}`), user }
   const res = {
     status: jest.fn().mockReturnThis(),
     json: jest.fn((data) => data)
   }
   const next = jest.fn()
-  it.skip('user exist', async () => {})
+
+  it('user exist', async () => {
+      passport.authenticate = jest.fn(
+        (strategy, options, cb) => (req, res, next) => {
+          cb(null, user)
+        }
+      )
+      guard(req, res, next)
+      expect(next).toHaveBeenCalled()
+  })
+    
+      it('user not exist', async () => {
+        passport.authenticate = jest.fn((strategy, options, cb) => (req, res, next) => {
+          cb(null, false)
+        },)
+        guard(req, res, next)
+          expect(req.get).toHaveBeenCalled()
+          expect(res.status).toHaveBeenCalled()
+          expect(res.json).toHaveBeenCalled()
+          expect(res.json).toHaveReturnedWith({
+            status: 'error',
+            code: HttpCode.UNAUTHORIZED,
+            message: 'Invalid credentials'
+          })
+      })
+    
+          it('user wrong user', async () => {
+            passport.authenticate = jest.fn(
+              (strategy, options, cb) => (req, res, next) => {
+                cb(null, {token: '2345'})
+              }
+            )
+            guard(req, res, next)
+            expect(req.get).toHaveBeenCalled()
+            expect(res.status).toHaveBeenCalled()
+            expect(res.json).toHaveBeenCalled()
+            expect(res.json).toHaveReturnedWith({
+              status: 'error',
+              code: HttpCode.UNAUTHORIZED,
+              message: 'Invalid credentials'
+            })
+          })
 })
